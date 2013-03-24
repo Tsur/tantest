5544fcc7da2d42a5c9659c10329dc76279320637
Zurisadai <prucheta@gmail.com>
2013-03-24 20:52:46 +0100

    login

--- src/com/scripturesos/tantest/ContactListAdapter.java
+++ src/com/scripturesos/tantest/ContactListAdapter.java
@@ -1,9 +1,15 @@
 package com.scripturesos.tantest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,34 +51,82 @@ public class ContactListAdapter extends BaseAdapter
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) 
 	{
-		View vi=convertView;
-		         
-		if(convertView == null)
+		View vi = convertView;
+		ViewHolder holder = null;
+		
+		if(vi == null || !( vi.getTag() instanceof ViewHolder))
 		{
 			LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			vi = inflater.inflate(R.layout.contacts_listview, null);
+			
+			//holder
+			holder = new ViewHolder();
+            holder.image = (ImageView) vi.findViewById(R.id.contacts_lv_img);
+            holder.name =(TextView) vi.findViewById(R.id.contacts_lv_name);
+            holder.status =  (TextView) vi.findViewById(R.id.contacts_lv_status);
+            holder.points = (TextView) vi.findViewById(R.id.contacts_lv_points);
+            //Save holder
+            vi.setTag(holder);
+		}
+		else
+		{
+			holder = (ViewHolder) vi.getTag();
 		}
 		             
 		ContactItemListView contact = contacts.get(position);
 		         
-		ImageView image = (ImageView) vi.findViewById(R.id.contacts_lv_img);
-		
-		//InputStream is = (InputStream) new URL(contact.getImg()).getContent();
-        //Drawable d = Drawable.createFromStream(is, contact.getImg());
-        //image.setImageDrawable(d);
-		 
-		int imageResource = activity.getResources().getIdentifier(contact.getImg(), null, activity.getPackageName());
-		image.setImageDrawable(activity.getResources().getDrawable(imageResource));
+		//ImageView image = (ImageView) vi.findViewById(R.id.contacts_lv_img);
+
+		//Save image to cache
+        if(Cache.images.get(position) == null)
+        {
+            InputStream is;
+            Drawable img = null;
+			try 
+			{
+				is = (InputStream) new URL(contact.getImg()).getContent();
+				img = Drawable.createFromStream(is, contact.getImg());
+			} 
+			catch (MalformedURLException e) 
+			{
+				// TODO Auto-generated catch block
+				img = activity.getResources().getDrawable(R.drawable.next_arrow);
+			} 
+			catch (IOException e) 
+			{
+				// TODO Auto-generated catch block
+				img = activity.getResources().getDrawable(R.drawable.next_arrow);
+			}
+            
+            Cache.images.put(position, img);
+        }
+        
+		//int imageResource = activity.getResources().getIdentifier(contact.getImg(), null, activity.getPackageName());
+		//holder.image.setImageDrawable(activity.getResources().getDrawable(imageResource));
+        holder.image.setImageDrawable(Cache.images.get(position));
 		         
-		TextView name = (TextView) vi.findViewById(R.id.contacts_lv_name);
-		name.setText(contact.getName());
+		//TextView name = (TextView) vi.findViewById(R.id.contacts_lv_name);
+		holder.name.setText(contact.getName());
 		         
-		TextView status = (TextView) vi.findViewById(R.id.contacts_lv_status);
-		status.setText(contact.getStatus());
+		//TextView status = (TextView) vi.findViewById(R.id.contacts_lv_status);
+		holder.status.setText(contact.getStatus());
 		
-		TextView points = (TextView) vi.findViewById(R.id.contacts_lv_points);
-		points.setText(contact.getPoints());
+		//TextView points = (TextView) vi.findViewById(R.id.contacts_lv_points);
+		holder.points.setText(contact.getPoints());
 		 
 		return vi;
 	  }
+	
+	 class ViewHolder
+	 {
+	        TextView name;
+	        TextView status;
+	        TextView points;
+	        ImageView image;
+	 }
+	 
+	 static class Cache
+	 {
+	 	static SparseArray<Drawable> images = new SparseArray<Drawable>();
+	 }
 }
--- src/com/scripturesos/tantest/MainActivity.java
+++ src/com/scripturesos/tantest/MainActivity.java
@@ -1,18 +1,25 @@
 package com.scripturesos.tantest;
 
-import android.content.Intent;
+import java.util.Locale;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
 
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
+public class MainActivity extends Activity 
+{
 
-public class MainActivity extends ActionBarActivity {
-
-	private ProgressBar loader;
-
+	private ListView countriesContainer;
+	private Button country;
+	private String abbr;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -20,102 +27,100 @@ public class MainActivity extends ActionBarActivity {
 		/* INIT CONTENT VIEW */
 		setContentView(R.layout.activity_main);
 		
-		loader = (ProgressBar) findViewById(R.id.main_progressbar);
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-	
-
-    @Override
-    public void onResume()
-    {
-    	if(loader != null)
+		country = (Button) findViewById(R.id.main_country);
+		
+		countriesContainer = (ListView) findViewById(R.id.main_lv);
+		
+		countriesContainer.setAdapter(new CountryListAdapter(this));
+		
+		countriesContainer.setOnItemClickListener(new OnItemClickListener()
 		{
-    		loader.setVisibility(View.GONE);
-		}
-    	
-    	super.onResume();
-    }
+		    @SuppressWarnings("deprecation")
+			@TargetApi(16)
+			@Override 
+			public void onItemClick(AdapterView<?> arg0, View view,int position, long arg3)
+		    { 
+		    	abbr = MainActivity.abbreviations[position];
+		    	
+		    	//abbr = Locale.getISOCountries()[position];
+		    	countriesContainer.setVisibility(View.GONE);
+		    	
+		    	Log.i("tantes","nuevo pais es: "+abbr);
+		    	
+		    	if (Build.VERSION.SDK_INT >= 16)
+		    		country.setBackground(CountryListAdapter.Cache.images.get(position));
+		    	else
+		    		country.setBackgroundDrawable(CountryListAdapter.Cache.images.get(position));
+		    }
+		});
+	}
 	
-	public boolean onOptionsItemSelected(MenuItem item)
+	public void displayCountries(View view)
 	{
+		assert countriesContainer != null;
 		
-		loader.setVisibility(View.VISIBLE);
-		loader.setIndeterminate(true);
-		
-		switch(item.getItemId())
-		{
-			case R.id.menu_header_test:
-				
-				//Mostramos ajax cargando
-				
-				Log.i("tantes","creando actividad");
-
-				
-				//RelativeLayout rl = (RelativeLayout) findViewById(R.id.main_logoimg);
-				//loader = new MYGIFView(this);
-				//rl.addView(loader);
-				
-				Intent intent = new Intent(this, TestActivity.class);
-				Log.i("tantes","iniciando actividad");
-				startActivity(intent);
-				
-
-				break;
-			case R.id.menu_header_social:
-				Intent cintent = new Intent(this, ContactsActivity.class);
-				Log.i("tantes","iniciando actividad");
-				startActivity(cintent);
-				break;
-			case R.id.menu_settings:
-
-				break;
-			default:
-		}
-		
-		return true;
+		if(countriesContainer.isShown())
+	    {
+		   countriesContainer.setVisibility(View.GONE);
+	    }
+	    else
+	    {
+		   countriesContainer.setVisibility(View.VISIBLE);
+	    }
 	}
 	
-	/*private class MYGIFView extends View{
-
-		Movie movie;
-		InputStream is=null;
-		long moviestart;
-		
-		public MYGIFView(Context context)
-		{
-			super(context);
-
-			is=context.getResources().openRawResource(R.drawable.loader);
-			movie=Movie.decodeStream(is);
-		}
-
-		@Override
-		protected void onDraw(Canvas canvas)
-		{
-
-			canvas.drawColor(Color.TRANSPARENT);
-			super.onDraw(canvas);
-			long now = android.os.SystemClock.uptimeMillis();
-
-			if(moviestart == 0)
-			{ 
-				// first time
-				moviestart = now;
-			}
-			
-			int relTime = (int)((now - moviestart) % movie.duration()) ;
-		
-			movie.setTime(relTime);
-			movie.draw(canvas,this.getWidth()/2-11,this.getHeight()/2+27);
-			this.invalidate();
-		}
-	}*/
-
+	public static String[] countries =
+	{ 
+		"Andorra","(al-Imārāt) الامارات","(Afganistan) افغانستان", "ANTIGUA AND BARBUDA", "ANGUILLA","AL","AM", "AN", "AO", "AQ", 
+		"AR","AS", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", 
+		"BF","BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BR",
+        "BS", "BT", "BW", "BY", "BZ", "CA", "CC","CD", "CF", "CG",
+        "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW",
+        "CX", "CY", "CZ", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ",
+        "EC", "EE", "EG", "EH", "ER", "ES", "ET", "EU", "FI", "FJ", "FK",
+        "FM", "FO", "FR", "GA", "GB", "GD", "GE", "GG", "GH", "GI", "GL",
+        "GM", "GN", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HN",
+        "HR", "HT", "HU", "IC", "ID", "IE", "IL", "IM", "IN", "IQ", "IR",
+        "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM",
+        "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK",
+        "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF",
+        "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS",
+        "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF",
+        "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE",
+        "PF", "PG", "PH", "PK", "PL", "PN", "PR", "PS", "PT", "PW", "PY",
+        "QA", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG",
+        "SH", "SI", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV",
+        "SY", "SZ", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", 
+        "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "US", "UY",
+        "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE",
+        "YT", "ZA","ZM","ZW"
+    };
+	
+	//Usar mejor Locale.getISOCountries()
+	public static String[] abbreviations = 
+	{ 
+		"AD","AE","AF", "AG", "AI","AL","AM", "AN", "AO", "AQ", 
+		"AR","AS", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", 
+		"BF","BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BR",
+        "BS", "BT", "BW", "BY", "BZ", "CA", "CC","CD", "CF", "CG",
+        "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW",
+        "CX", "CY", "CZ", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO2", "DZ",
+        "EC", "EE", "EG", "EH", "ER", "ES", "ET", "EU", "FI", "FJ", "FK",
+        "FM", "FO", "FR", "GA", "GB", "GD", "GE", "GG", "GH", "GI", "GL",
+        "GM", "GN", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HN",
+        "HR", "HT", "HU", "IC", "ID", "IE", "IL", "IM", "IN", "IQ", "IR",
+        "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM",
+        "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK",
+        "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF",
+        "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS",
+        "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF",
+        "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE",
+        "PF", "PG", "PH", "PK", "PL", "PN", "PR", "PS", "PT", "PW", "PY",
+        "QA", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG",
+        "SH", "SI", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV",
+        "SY", "SZ", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", 
+        "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "US", "UY",
+        "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE",
+        "YT", "ZA","ZM","ZW"
+     };
 }
